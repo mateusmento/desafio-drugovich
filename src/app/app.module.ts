d2645c795ba0d994c8674c9f51908b644e6b6647
Mateus Sarmento <omateusmento@gmail.com>
2022-12-11 03:23:54 -0300

    Import HttpClientModule in AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ClientsViewComponent } from './views/clients-view/clients-view.component';
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { ClientsViewComponent } from './views/clients-view/clients-view.componen
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
