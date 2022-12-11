0f586a52cfe6017053340c706f041007b736bb27
Mateus Sarmento <omateusmento@gmail.com>
2022-12-11 03:34:46 -0300

    Run linting

--- src/app/service/client.service.ts
+++ src/app/service/client.service.ts
@@ -1,12 +1,12 @@
-import { HttpClient } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { Client } from "../model/client.model";
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Client } from '../model/client.model';
 
 @Injectable()
 export class ClientService {
-    constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {}
 
-    fetchClients() {
-      return this.http.get<Client[]>("https://demo4231135.mockable.io/");
-    }
+  fetchClients() {
+    return this.http.get<Client[]>('https://demo4231135.mockable.io/');
+  }
 }
