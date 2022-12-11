435babe2f321ebd838dfa6d9fea653a5461dfbef
Mateus Sarmento <omateusmento@gmail.com>
2022-12-11 08:35:55 -0300

    Fetch clients from mockapi

--- src/app/service/client.service.ts
+++ src/app/service/client.service.ts
@@ -5,34 +5,12 @@ import { Client } from '../model/client.model';
 
 @Injectable()
 export class ClientService {
+  private baseUrl = "https://63966e4a90ac47c68085cb9c.mockapi.io";
+
   constructor(private http: HttpClient) {}
 
   fetchClients(): Observable<Client[]> {
-    return new BehaviorSubject([
-      {
-        id: 1,
-        name: 'Auto-Peças Joaquim LTDA.',
-        cnpj: '000000000000000',
-        status: 'Inativo',
-      },
-      {
-        id: 2,
-        name: 'Auto-Peças Carlinhos LTDA.',
-        cnpj: '000000000000000',
-        status: 'Ativo',
-      },
-      {
-        id: 3,
-        name: 'Auto-Peças João LTDA.',
-        cnpj: '000000000000000',
-        status: 'Ativo',
-      },
-      {
-        id: 4,
-        name: 'Auto-Peças Maria LTDA.',
-        cnpj: '000000000000000',
-        status: 'Ativo',
-      },
-    ]);
+    return this.http.get<Client[]>(this.baseUrl + "/clients");
   }
+
 }
