7606ee9f6b8cd611dff28d2f8dd3a1c666d21639
Mateus Sarmento <omateusmento@gmail.com>
2022-12-11 03:18:41 -0300

    Add clients-view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClientsViewComponent } from './views/clients-view/clients-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientsViewComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/views/clients-view/clients-view.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-clients-view',
+  templateUrl: './clients-view.component.html',
+  styleUrls: ['./clients-view.component.scss']
+})
+export class ClientsViewComponent {
+
+}
