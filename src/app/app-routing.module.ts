955a7a4cb4480dc85ce55ffdfe686182a571648d
Mateus Sarmento <omateusmento@gmail.com>
2022-12-11 03:18:51 -0300

    Add route for clients view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ClientsViewComponent } from './views/clients-view/clients-view.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: "clients", pathMatch: "full" },
+  { path: "clients", component: ClientsViewComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
