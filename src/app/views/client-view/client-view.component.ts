d1b33ca1b5dac41907d0e046aa5782beafdc1a70
Mateus Sarmento <omateusmento@gmail.com>
2022-12-11 08:38:52 -0300

    Save new client on submit

--- src/app/views/client-view/client-view.component.ts
+++ src/app/views/client-view/client-view.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { ClientStatus } from 'src/app/model/client.model';
+import { ClientService } from 'src/app/service/client.service';
 
 @Component({
   selector: 'app-client-view',
@@ -13,4 +14,11 @@ export class ClientViewComponent {
     cnpj: new FormControl("", { nonNullable: true }),
     status: new FormControl<ClientStatus>("Ativo", { nonNullable: true }),
   })
+
+  constructor(private clientService: ClientService) {}
+
+  saveClient() {
+    this.clientService.createClient(this.client.getRawValue())
+      .subscribe(res => this.client.setValue(res));
+  }
 }
