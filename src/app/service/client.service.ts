d2f7ae6da7752619ee57aec6e9ae22f8b90cadf0
Mateus Sarmento <omateusmento@gmail.com>
2022-12-11 04:50:17 -0300

    Mock clients data

--- src/app/service/client.service.ts
+++ src/app/service/client.service.ts
@@ -1,12 +1,38 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, map, Observable } from 'rxjs';
 import { Client } from '../model/client.model';
 
 @Injectable()
 export class ClientService {
   constructor(private http: HttpClient) {}
 
-  fetchClients() {
-    return this.http.get<Client[]>('https://demo4231135.mockable.io/');
+  fetchClients(): Observable<Client[]> {
+    return new BehaviorSubject([
+      {
+        id: 1,
+        name: 'Auto-Peças Joaquim LTDA.',
+        cnpj: '000000000000000',
+        status: 'Inativo',
+      },
+      {
+        id: 2,
+        name: 'Auto-Peças Carlinhos LTDA.',
+        cnpj: '000000000000000',
+        status: 'Ativo',
+      },
+      {
+        id: 3,
+        name: 'Auto-Peças João LTDA.',
+        cnpj: '000000000000000',
+        status: 'Ativo',
+      },
+      {
+        id: 4,
+        name: 'Auto-Peças Maria LTDA.',
+        cnpj: '000000000000000',
+        status: 'Ativo',
+      },
+    ]);
   }
 }
