bc980e935e97988ee12ab194a82172d6823990d6
Mateus Sarmento <omateusmento@gmail.com>
2022-12-11 08:39:45 -0300

    Add topbar ui component

---
+++ src/app/ui/topbar/topbar.component.ts
@@ -0,0 +1,11 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-topbar',
+  templateUrl: './topbar.component.html',
+  styleUrls: ['./topbar.component.scss'],
+  encapsulation: ViewEncapsulation.ShadowDom
+})
+export class TopbarComponent {
+
+}
